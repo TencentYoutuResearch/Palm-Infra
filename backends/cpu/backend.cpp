a0a50d2b2890e499b56980dda40c11c8a130e175
Molly Sophia <mollysophia379@gmail.com>
2026-09-12 20:02:19 +0800

    refactor: extract CPU matmul dispatch

--- backends/cpu/backend.cpp
+++ backends/cpu/backend.cpp
@@ -1,6 +1,7 @@
 #include "backends/cpu/backend.h"
 #include "backends/cpu/deepseek_v4_ops.h"
 #include "backends/cpu/layout_ops.h"
+#include "backends/cpu/matmul_ops.h"
 #include "backends/cpu/normalization_ops.h"
 #include "backends/cpu/recurrent_ops.h"
 #include "graph/graph.h"
@@ -56,26 +57,9 @@ void CPUBackend::dispatch(const GraphNode& node,
         break;
 
     case OpType::MATMUL:
-        if (inputs.size() >= 2 && inputs[0] && inputs[1] && output) {
-            // Fused activation: params.i32[0]=activation, [1]=act_n_begin, [2]=act_n_len.
-            // Default: NONE, 0, -1 (whole output).
-            Activation act = (Activation)graph_params::get_i32(params, 0, 0);
-            int act_n_begin = graph_params::get_i32(params, 1, 0);
-            int act_n_len = graph_params::get_i32(params, 2, -1);
-            kernel_matmul_fp32(*inputs[0], *inputs[1], *output, thread_pool,
-                                act, act_n_begin, act_n_len);
-        }
-        break;
-
     case OpType::MATMUL_BATCH:
-        if (output)
-            kernel_matmul_batch(inputs, *output, thread_pool);
-        break;
-
     case OpType::GEMV_SPARSE_A:
-        if (inputs.size() >= 2 && inputs[0] && inputs[1] && output) {
-            kernel_gemv_sparse_a(*inputs[0], *inputs[1], *output, thread_pool);
-        }
+        dispatch_cpu_matmul(node, inputs, output, thread_pool);
         break;
 
     case OpType::RMS_NORM_ROPE:
---
+++ backends/cpu/matmul_ops.cpp
@@ -0,0 +1,36 @@
+#include "backends/cpu/matmul_ops.h"
+
+#include "kernels/cpu/matmul/matmul.h"
+
+void dispatch_cpu_matmul(
+        const GraphNode& node, const std::vector<const Tensor*>& inputs,
+        Tensor* output, ThreadPool* thread_pool) {
+    const OpParams& params = node.params;
+    switch (node.op_type) {
+    case OpType::MATMUL:
+        if (inputs.size() >= 2 && inputs[0] && inputs[1] && output) {
+            // Fused activation: params.i32[0]=activation, [1]=act_n_begin, [2]=act_n_len.
+            // Default: NONE, 0, -1 (whole output).
+            Activation act = (Activation)graph_params::get_i32(params, 0, 0);
+            int act_n_begin = graph_params::get_i32(params, 1, 0);
+            int act_n_len = graph_params::get_i32(params, 2, -1);
+            kernel_matmul_fp32(*inputs[0], *inputs[1], *output, thread_pool,
+                                act, act_n_begin, act_n_len);
+        }
+        break;
+
+    case OpType::MATMUL_BATCH:
+        if (output)
+            kernel_matmul_batch(inputs, *output, thread_pool);
+        break;
+
+    case OpType::GEMV_SPARSE_A:
+        if (inputs.size() >= 2 && inputs[0] && inputs[1] && output) {
+            kernel_gemv_sparse_a(*inputs[0], *inputs[1], *output, thread_pool);
+        }
+        break;
+
+    default:
+        break;
+    }
+}
